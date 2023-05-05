fc798aa145f7928461368a417773ac5c27acad21
Ahmad Jazmawi <ahmad_jaz@hotmail.com>
2023-05-05 17:11:37 -0400

    Added zoom in/out functionality and scroll bar, moved rotation buttons to dropdown menu

--- ImageEditor2/Form1.Designer.cs
+++ ImageEditor2/Form1.Designer.cs
@@ -28,36 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.insertImage = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.label1 = new System.Windows.Forms.Label();
-            this.right90 = new System.Windows.Forms.Button();
-            this.left90 = new System.Windows.Forms.Button();
-            this.rotate180 = new System.Windows.Forms.Button();
-            this.flipVert = new System.Windows.Forms.Button();
-            this.flipHoriz = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.MenuItemOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemSaveas = new System.Windows.Forms.ToolStripMenuItem();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.toolStripDropDownButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.MenuItemRotateRight90 = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemRotateLeft90 = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemRotate180 = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemFlipVertical = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemFlipHorizontal = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(0, 137);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1292, 537);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // insertImage
-            // 
-            this.insertImage.Location = new System.Drawing.Point(24, 55);
-            this.insertImage.Name = "insertImage";
-            this.insertImage.Size = new System.Drawing.Size(129, 50);
-            this.insertImage.TabIndex = 1;
-            this.insertImage.Text = "Insert image";
-            this.insertImage.UseVisualStyleBackColor = true;
-            this.insertImage.Click += new System.EventHandler(this.insert_Click);
             // 
             // openFileDialog1
             // 
@@ -65,89 +56,145 @@
             this.openFileDialog1.Tag = "hello";
             this.openFileDialog1.Title = "HELLO";
             // 
-            // label1
+            // toolStrip1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(559, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 16);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Image Rotations";
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripDropDownButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1184, 27);
+            this.toolStrip1.TabIndex = 12;
+            this.toolStrip1.Text = "toolStrip1";
             // 
-            // right90
+            // toolStripDropDownButton1
             // 
-            this.right90.Location = new System.Drawing.Point(350, 55);
-            this.right90.Name = "right90";
-            this.right90.Size = new System.Drawing.Size(139, 23);
-            this.right90.TabIndex = 7;
-            this.right90.Text = "Rotate right 90*";
-            this.right90.UseVisualStyleBackColor = true;
-            this.right90.Click += new System.EventHandler(this.right90_Click);
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemOpen,
+            this.MenuItemSave,
+            this.MenuItemSaveas});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(46, 24);
+            this.toolStripDropDownButton1.Text = "File";
             // 
-            // left90
+            // MenuItemOpen
             // 
-            this.left90.Location = new System.Drawing.Point(350, 95);
-            this.left90.Name = "left90";
-            this.left90.Size = new System.Drawing.Size(139, 23);
-            this.left90.TabIndex = 8;
-            this.left90.Text = "Rotate left 90*";
-            this.left90.UseVisualStyleBackColor = true;
-            this.left90.Click += new System.EventHandler(this.left90_Click);
+            this.MenuItemOpen.Name = "MenuItemOpen";
+            this.MenuItemOpen.Size = new System.Drawing.Size(224, 26);
+            this.MenuItemOpen.Text = "Open";
+            this.MenuItemOpen.Click += new System.EventHandler(this.MenuItemOpen_Click);
             // 
-            // rotate180
+            // MenuItemSave
             // 
-            this.rotate180.Location = new System.Drawing.Point(535, 55);
-            this.rotate180.Name = "rotate180";
-            this.rotate180.Size = new System.Drawing.Size(145, 23);
-            this.rotate180.TabIndex = 9;
-            this.rotate180.Text = "Rotate 180*";
-            this.rotate180.UseVisualStyleBackColor = true;
-            this.rotate180.Click += new System.EventHandler(this.rotate180_Click);
+            this.MenuItemSave.Name = "MenuItemSave";
+            this.MenuItemSave.Size = new System.Drawing.Size(224, 26);
+            this.MenuItemSave.Text = "Save";
+            this.MenuItemSave.Click += new System.EventHandler(this.Save_Click);
             // 
-            // flipVert
+            // MenuItemSaveas
             // 
-            this.flipVert.Location = new System.Drawing.Point(535, 95);
-            this.flipVert.Name = "flipVert";
-            this.flipVert.Size = new System.Drawing.Size(145, 23);
-            this.flipVert.TabIndex = 10;
-            this.flipVert.Text = "Flip Vertical";
-            this.flipVert.UseVisualStyleBackColor = true;
-            this.flipVert.Click += new System.EventHandler(this.flipVert_Click);
+            this.MenuItemSaveas.Name = "MenuItemSaveas";
+            this.MenuItemSaveas.Size = new System.Drawing.Size(224, 26);
+            this.MenuItemSaveas.Text = "Save As";
+            this.MenuItemSaveas.Click += new System.EventHandler(this.Saveas_Click);
             // 
-            // flipHoriz
+            // trackBar1
             // 
-            this.flipHoriz.Location = new System.Drawing.Point(717, 54);
-            this.flipHoriz.Name = "flipHoriz";
-            this.flipHoriz.Size = new System.Drawing.Size(138, 23);
-            this.flipHoriz.TabIndex = 11;
-            this.flipHoriz.Text = "Flip Horizontal";
-            this.flipHoriz.UseVisualStyleBackColor = true;
-            this.flipHoriz.Click += new System.EventHandler(this.flipHoriz_Click);
+            this.trackBar1.Location = new System.Drawing.Point(59, 81);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(999, 56);
+            this.trackBar1.TabIndex = 15;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
-            // textBox1
+            // toolStripDropDownButton2
             // 
-            this.textBox1.Location = new System.Drawing.Point(1044, 69);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 5;
+            this.toolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemRotateRight90,
+            this.MenuItemRotateLeft90,
+            this.MenuItemRotate180,
+            this.MenuItemFlipVertical,
+            this.MenuItemFlipHorizontal});
+            this.toolStripDropDownButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton2.Image")));
+            this.toolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton2.Name = "toolStripDropDownButton2";
+            this.toolStripDropDownButton2.Size = new System.Drawing.Size(67, 24);
+            this.toolStripDropDownButton2.Text = "Rotate";
+            // 
+            // MenuItemRotateRight90
+            // 
+            this.MenuItemRotateRight90.Name = "MenuItemRotateRight90";
+            this.MenuItemRotateRight90.Size = new System.Drawing.Size(224, 26);
+            this.MenuItemRotateRight90.Text = "Rotate right 90*";
+            this.MenuItemRotateRight90.Click += new System.EventHandler(this.MenuItemRotateRight90_Click);
+            // 
+            // MenuItemRotateLeft90
+            // 
+            this.MenuItemRotateLeft90.Name = "MenuItemRotateLeft90";
+            this.MenuItemRotateLeft90.Size = new System.Drawing.Size(224, 26);
+            this.MenuItemRotateLeft90.Text = "Rotate left 90*";
+            this.MenuItemRotateLeft90.Click += new System.EventHandler(this.MenuItemRotateLeft90_Click);
+            // 
+            // MenuItemRotate180
+            // 
+            this.MenuItemRotate180.Name = "MenuItemRotate180";
+            this.MenuItemRotate180.Size = new System.Drawing.Size(224, 26);
+            this.MenuItemRotate180.Text = "Rotate 180*";
+            this.MenuItemRotate180.Click += new System.EventHandler(this.MenuItemRotate180_Click);
+            // 
+            // MenuItemFlipVertical
+            // 
+            this.MenuItemFlipVertical.Name = "MenuItemFlipVertical";
+            this.MenuItemFlipVertical.Size = new System.Drawing.Size(224, 26);
+            this.MenuItemFlipVertical.Text = "Flip Vertical";
+            this.MenuItemFlipVertical.Click += new System.EventHandler(this.MenuItemFlipVertical_Click);
+            // 
+            // MenuItemFlipHorizontal
+            // 
+            this.MenuItemFlipHorizontal.Name = "MenuItemFlipHorizontal";
+            this.MenuItemFlipHorizontal.Size = new System.Drawing.Size(224, 26);
+            this.MenuItemFlipHorizontal.Text = "Flip Horizontal";
+            this.MenuItemFlipHorizontal.Click += new System.EventHandler(this.MenuItemFlipHorizontal_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(36, 153);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1074, 495);
+            this.panel1.TabIndex = 16;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.AccessibleName = " ";
+            this.pictureBox1.Location = new System.Drawing.Point(20, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1002, 433);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1292, 686);
-            this.Controls.Add(this.flipHoriz);
-            this.Controls.Add(this.flipVert);
-            this.Controls.Add(this.rotate180);
-            this.Controls.Add(this.left90);
-            this.Controls.Add(this.right90);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.insertImage);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(1184, 686);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.toolStrip1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -155,17 +202,21 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button insertImage;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button right90;
-        private System.Windows.Forms.Button left90;
-        private System.Windows.Forms.Button rotate180;
-        private System.Windows.Forms.Button flipVert;
-        private System.Windows.Forms.Button flipHoriz;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemOpen;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemSave;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemSaveas;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton2;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemRotateRight90;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemRotateLeft90;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemRotate180;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFlipVertical;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFlipHorizontal;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- ImageEditor2/Form1.cs
+++ ImageEditor2/Form1.cs
@@ -20,31 +20,96 @@ namespace ImageEditor2
         }
         private Image Img;
         private Size OriginalImageSize;
-
-
+        private Bitmap bitmap;                                          // bitmap object of the image file we want to display
+        private float zoomFactor = 1.00F;                               // zoom factor, used when changing image dimensions
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+       
 
-        private void insert_Click(object sender, EventArgs e)
+       
+
+        
+
+        private void MenuItemOpen_Click(object sender, EventArgs e)
         {
-
-            
-
             OpenFileDialog open = new OpenFileDialog();
             // image filters  
-            
+
             if (open.ShowDialog() == DialogResult.OK)
             {
                 // display image in picture box  
                 pictureBox1.Image = new Bitmap(open.FileName);
+                Img = pictureBox1.Image;
                 // image file path  
-                textBox1.Text = open.FileName;
+                
+            }
+
+
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Saveas_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        
+        Image Zoom(Image img, Size size)
+        {
+            Bitmap bmp = new Bitmap(img, (img.Width * size.Width),  (img.Height * size.Height));
+            Graphics g = Graphics.FromImage(bmp);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bmp;
+        }
+        private void getZoom(int zoomSize)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            int newWidth = pictureBox1.Image.Width / zoomSize;
+            int newHeight = pictureBox1.Image.Height / zoomSize;
+            Bitmap bmpNew = new Bitmap(newWidth, newHeight);
+
+            Graphics gr = Graphics.FromImage(bmpNew);
+            gr.DrawImage(bmp, 0, 0, bmpNew.Width, bmpNew.Height);
+            pictureBox1.Image = bmpNew;
+
+            //panel1.AutoScrollMinSize = new Size(pictureBox1.Image.Width, pictureBox1.Image.Height);
+
+
+        }
+
+        
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar1.Value > 0)
+            {
+                pictureBox1.Image = null;
+                pictureBox1.Image = Zoom(Img, new Size(trackBar1.Value, trackBar1.Value));
+
             }
         }
 
-        private void left90_Click(object sender, EventArgs e)
+        private void MenuItemRotateRight90_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                return;
+            }
+            else
+            {
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void MenuItemRotateLeft90_Click(object sender, EventArgs e)
         {
             if (pictureBox1.Image == null)
             {
@@ -57,40 +122,22 @@ namespace ImageEditor2
             }
         }
 
-        private void right90_Click(object sender, EventArgs e)
-        {
-            if(pictureBox1.Image == null)
-            {
-                return;
-            }
-            else
-            {
-                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
-                pictureBox1.Refresh();
-            }
-            
-
-        }
-
-        private void rotate180_Click(object sender, EventArgs e)
+        private void MenuItemRotate180_Click(object sender, EventArgs e)
         {
             pictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
             pictureBox1.Refresh();
-
         }
 
-        private void flipVert_Click(object sender, EventArgs e)
+        private void MenuItemFlipVertical_Click(object sender, EventArgs e)
         {
             pictureBox1.Image.RotateFlip(RotateFlipType.RotateNoneFlipY);
             pictureBox1.Refresh();
-
         }
 
-        private void flipHoriz_Click(object sender, EventArgs e)
+        private void MenuItemFlipHorizontal_Click(object sender, EventArgs e)
         {
             pictureBox1.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
             pictureBox1.Refresh();
-
         }
     }
 }
